d582b41a4ac0f0a9a38c56a7a453556d38d8d0b0
Janakarajan Natarajan <janakan@amazon.com>
2021-01-29 21:15:32 +0000

    Build aarch64 wheels

--- azure-pipelines/build.bash
+++ azure-pipelines/build.bash
@@ -3,4 +3,5 @@ set -ex
 python -m pip install --upgrade pip
 pip install cibuildwheel==$CIBUILDWHEEL_VERSION || \
     pip install git+git://github.com/joerick/cibuildwheel.git@$CIBUILDWHEEL_VERSION
+export CIBW_ARCHS_LINUX="auto aarch64"
 cibuildwheel --output-dir wheelhouse .
